18aec624f122bdf0b912d11b235ab01e4f8203c3
SavioR0 <saviorodrigues012@gmail.com>
2023-05-11 21:44:00 -0300

    Adição na pasta store

--- iniciando_javascript/controle.js
+++ iniciando_javascript/controle.js
@@ -1,60 +1,117 @@
 var pagina = -1
 var elementos = 0
 
-/* alert('teste') */
-class Pessoa {
-    constructor(nome, email, telefone) {
-        this.nome = nome
-        this.email = email
-        this.telefone = telefone
+function Pessoa(nome, email, telefone) {
+    this.nome = nome;
+    this.email = email;
+    this.telefone = telefone;
+  }
+
+var listaPessoas = []
+
+function Proximo() { 
+    if(elementos>0 && pagina<(elementos-1)){
+        pagina++
+        Exibir()
+    }
+    else{
+        alert("Não existe uma proxima pagina")
     }
 }
 
 
+function Cadastrar() {
 
 
-var listaPessoas = []
-function Cadastrar(){
     var nome = document.getElementById("nomeid");
     var telefone = document.getElementById("telefoneid");
     var email = document.getElementById("emailid");
 
-    if(nome.value !="" && telefone.value!="" && email.value!=""){
-        var novo = new Pessoa(nome.value, email.value, telefone.value)
-        listaPessoas.push(novo)
-        alert("TODOS OS CAMPOS PREENCHIDOS COM SUCESSO")
-        document.getElementById("nomeid").value=""
-        document.getElementById("emailid").value=""
-        document.getElementById("telefoneid").value=""
+    var aux_Nome = nome.value
+    var aux_Telefone = telefone.value
+    var aux_Email = email.value
+    if(nome.value!="" && email.value!="" && telefone.value!=""){
+        var novo = new Pessoa(nome.value,email.value,telefone.value);
+        listaPessoas.push(novo);
+        document.getElementById("nomeid").value = '';
+        document.getElementById("telefoneid").value = '';
+        document.getElementById("emailid").value = '';
+        
+        alert("Dados cadastrados com sucesso!")
+        var formulario = document.getElementById("formid")
+
+        if(elementos==0){
+            var botao_Ant = document.createElement("button");
+            botao_Ant.type = 'button';   
+            botao_Ant.class = "anterior"
+            botao_Ant.setAttribute("onclick","Anterior()")
+            botao_Ant.textContent = "Anterior"
+            formulario.appendChild(botao_Ant)
+
+            var botao_Prox = document.createElement("button");
+            botao_Prox.type = 'button';   
+            botao_Prox.class = "proximo"
+            botao_Prox.setAttribute("onclick","Proximo()")
+            botao_Prox.textContent = "Proximo"
+            formulario.appendChild(botao_Prox)   
+            
+            var tabela = document.createElement("table")
+            tabela.id = "tabelaid"
+            var linha = document.createElement("tr")
+            var coluna = document.createElement("td")
+            var coluna1 = document.createElement("td")
+            var coluna2 = document.createElement("td")
+            coluna.textContent = "Nome"
+            linha.appendChild(coluna)
+            coluna1.textContent = "Telefone"
+            linha.appendChild(coluna1)
+            coluna2.textContent = "Email"
+            linha.appendChild(coluna2)
+            tabela.appendChild(linha)
+            tabela.className = "form_login"
+            document.body.appendChild(tabela) 
+        }
+
+        var aux_Tabela = document.getElementById("tabelaid") 
+        var linha = document.createElement("tr")
+        var coluna = document.createElement("td")
+        var coluna1 = document.createElement("td")
+        var coluna2 = document.createElement("td")
+        coluna.textContent = aux_Nome
+        linha.appendChild(coluna)
+        coluna1.textContent = aux_Telefone
+        linha.appendChild(coluna1)
+        coluna2.textContent = aux_Email
+        linha.appendChild(coluna2)
+        aux_Tabela.appendChild(linha)
+        
+        
         elementos++
         pagina = -1
-    }else{
-        alert("TODOS OS CAMPOS PRECISAM SER PREENCHIDOS")
     }
+    else{
+        alert("TODOS OS CAMPOS SÃO OBRIGATÓRIOS")
+    }
+}
+
+function Exibir() { 
     
+    document.getElementById("nomeid").value = listaPessoas[pagina].nome;
+    document.getElementById("telefoneid").value = listaPessoas[pagina].telefone;
+    document.getElementById("emailid").value = listaPessoas[pagina].email;
 }
 
-function Exibir(){
-    document.getElementById("nomeid").value = listaPessoas[pagina].nome
-    document.getElementById("telefoneid").value = listaPessoas[pagina].telefone
-    document.getElementById("emailid").value = listaPessoas[pagina].email
+function Limpar() { 
+    document.getElementById("nomeid").value = "";
+    document.getElementById("telefoneid").value = "";
+    document.getElementById("emailid").value = "";
 }
 
-function Anterior(){
-    if(elementos > 0 && pagina>0){
-        pagina--
-        Exibir()
+function Anterior() { 
+    if(elementos>0 && pagina>0){
+    pagina--
+    Exibir()
     }else{
-        alert("NÃO EXISTE ANTERIOR")
+        alert("Não existe uma pagina anterior")
     }
 }
-
-function Proximo(){
-    if(elementos > 0 && pagina<(elementos-1)){
-        pagina++
-        Exibir()
-    }else{
-        alert("NÃO EXISTE PROXIMO   ")
-    }
-}
-
